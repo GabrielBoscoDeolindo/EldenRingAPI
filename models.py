282ffafe76b5fec185cbaad67bfeb216b294d2a6
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-03-28 21:38:55 -0300

    todos os bosses adicionados

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from sqlmodel import SQLModel, Field
 class Boss(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str 
-    location: str
     photo: str
+    description: str
 
 def create_db_and_tables(engine):
     SQLModel.metadata.create_all(engine)
